184c7b44ee7afdffc884a7edf5b98db0aa74bbf3
Conner Anderson <cnnranderson@gmail.com>
2022-01-18 11:06:48 -0800

    Add dice roll function

--- scripts/Helpers.gd
+++ scripts/Helpers.gd
@@ -17,16 +17,23 @@ func chance_luck(percentage):
 		return chance in range(1, percentage)
 	return false
 
-func tile_to_world(tile_pos: Vector2, center_offset: bool = true) -> Vector2:
-	tile_pos *= Constants.TILE_SIZE
+func dice_roll(count: int = 1, sides: int = 6):
+	var sum = 0
+	for i in range(count):
+		var roll = randi() % sides + 1
+		sum += roll
+	return sum
+
+func tile_to_world(tpos: Vector2, center_offset: bool = true) -> Vector2:
+	tpos *= Constants.TILE_SIZE
 	if center_offset:
-		tile_pos += (Constants.TILE_V / 2)
-	return tile_pos
+		tpos += (Constants.TILE_V / 2)
+	return tpos
 
 func world_to_tile(world_pos: Vector2):
-	var tile_position = world_pos / float(Constants.TILE_SIZE)
-	tile_position = tile_position.floor()
-	return tile_position
+	var tpos = world_pos / float(Constants.TILE_SIZE)
+	tpos = tpos.floor()
+	return tpos
 
 func valid(obj):
 	if obj == null: return false
